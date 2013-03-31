74e140d6668bcc07caf1fe8bc2e4588bec7e3b7e
Cong <Cong@CongmatoiMac.local>
2013-03-31 19:13:22 +0800

    reverseList

--- reverseList/main.cpp
+++ reverseList/main.cpp
@@ -7,12 +7,67 @@
 //
 
 #include <iostream>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+void build(Node* &head, int n){
+    
+    head = new Node();
+    n--;
+    head->data = n;
+
+    Node* p = head;
+    while(n--){
+        p->next = new Node();
+        p = p->next;
+        p->data = n;
+    }
+}
+
+Node* reverseList(Node* head){
+    if(head == NULL){
+        cout<<"Invalid input"<<endl;
+        return NULL;
+    }
+    if(head->next == NULL)
+        return head;
+    
+    Node* p1 = head;
+    Node* p2 = p1->next;
+    Node* p3 = p2->next;
+    
+    p1->next = NULL;
+    while(p3 != NULL){
+        p2->next = p1;
+        p1 = p2;
+        p2 = p3;
+        p3 = p3->next;
+    }
+    
+    p2->next = p1;
+    return p2;
+}
+
+void print(Node* head){
+    while(head != NULL){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
 
 int main(int argc, const char * argv[])
 {
+    Node * head = NULL;
+    build(head, 10);
+    print(head);
+    print(reverseList(head));
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
 
